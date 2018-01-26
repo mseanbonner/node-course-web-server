14c00986e50d99e37906d610737c1c7d415c5438
M. Sean Bonner <sean@bonnerventure.com>
2018-01-25 19:54:41 -0800

    add projects page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'My Projects'
+  })
+});
+
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 12,
